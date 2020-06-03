e28f1dbdf8e0b2586dacf1e3889368112fccac83
himaja <himaja459@gmail.com>
2020-06-03 19:49:05 +1000

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage= () => (
+    <div className ='homepage'>
+        <div className= 'directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='titile'>
+                        HATS
+                    </h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='titile'>
+                        JACKETS
+                    </h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='titile'>
+                        SNEAKERS
+                    </h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='titile'>
+                        WOMENS
+                    </h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='titile'>
+                        MENS
+                    </h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
