6411999ae00ba3a51b1afb23d8129cedc9782938
himaja <himaja459@gmail.com>
2020-06-09 19:07:47 +1000

    update our menu item with path hats

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
-const Homepage= () => (
+const Homepage= ({history}) => (
     <div className ='homepage'>
-        <Directory />
+        <Directory/>
     </div>
 );
 
